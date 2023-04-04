4838846a515d69951cadba1779273bcacd4c07e5
Nana Araba Amissah Eshun <eshunnanaaraba@gmail.com>
2023-04-03 17:38:43 -0700

    loc a substr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,39 @@
 #include "main.h"
 
-
 /**
-* _strstr - a function that locates a substring.
-*@haystack : string to be searched
-*@needle : string to be located
-*Return: nothing
-*/
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: If the substring is located - a pointer to the beginning
+ *                                       of the located substring.
+ *         If the substring is not located - NULL.
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int x;
-	int y;
+	int index;
 
-	for (x = 0; haystack[x] != '\0'; x++)
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
 	{
-	for (y = 0; needle[y] != '\0'; y++)
-	{
-		if (haystack[x + y] != needle[y])
+		index = 0;
+
+		if (haystack[index] == needle[index])
 		{
-		break;
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
 	}
-	}
-	if (needle[y] == '\0')
-	{
-		(return) &haystack[x];
-	}
-	}
-	(return) NULL;
+
+	return ('\0');
 }
